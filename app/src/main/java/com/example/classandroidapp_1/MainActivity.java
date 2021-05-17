73b38c9bd8cf459362777b5ae1060361478811d6
nebel <merkelmller@yandex.ru>
2021-05-17 13:13:19 -0400

    There are some changes. We manage to fix the alignment of the widgets and make them centered both in regard to their layout and their specific viewgroup.
    Donw!

--- app/src/main/java/com/example/classandroidapp_1/MainActivity.java
+++ app/src/main/java/com/example/classandroidapp_1/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         truBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayTextView.setText("Bad answer!");
+                displayTextView.setText("Wrong answer!");
             }
         });
         falseBtn.setOnClickListener(new View.OnClickListener() {
